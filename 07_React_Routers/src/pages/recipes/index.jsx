a2359c641c354d8e7e039d800b3fe57b33e955f8
KunnuSherry <gamingvenom465@gmail.com>
2025-05-04 21:50:47 +0530

    useWindowSize Hook

--- 07_React_Routers/src/pages/recipes/index.jsx
+++ 07_React_Routers/src/pages/recipes/index.jsx
@@ -1,16 +1,19 @@
 import { useLocation } from "react-router-dom";
 import useFetch from "../../hooks/use-fetch";
+import useWindowResize from "../../window-resize/use-window-resize";
 
 function RecipeList() {
   const location = useLocation();
   const { data, loading, error } = useFetch("https://dummyjson.com/recipes");
+  const windowSize = useWindowResize()
 
   if (loading) return <h1>Fetching data, please wait...</h1>;
   if (error) return <h2>Error fetching data: {error.message}</h2>;
 
   return (
     <div>
-      <h1>Recipe list page</h1>
+      <h1 style={{color:windowSize.width<768?"red":"black"}}>Recipe list page</h1>
+      <h2>widht is {windowSize.width} and height is {windowSize.height}</h2>
       <ul>
         {data?.recipes?.length > 0
           ? data.recipes.map((recipeItem) => (
